4824e51a75a58ade29fedabf48e889c60e9350c2
Brian Coutts <bjhcoutts@gmail.com>
2022-01-19 17:47:41 -0800

    header and main image looking ok

--- src/components/indexPage/intro/IntroImage.svelte
+++ src/components/indexPage/intro/IntroImage.svelte
@@ -21,11 +21,20 @@
 		margin-top: -68.38px;
 		padding: calc(68.38px * 2) 0;
 		background-position: fixed;
+		position: relative;
 	}
 
 	section > * {
 		color: var(--white);
 		filter: var(--drop-shadow);
+	}
+
+	.details {
+		color: var(--accent);
+		position: absolute;
+		bottom: 0;
+		right:0;
+		opacity: 50%;
 	}
 
 </style>
@@ -35,5 +44,6 @@
 	<h1>Brian Coutts</h1>
 	<hr />
 	<h2>Web Dev ◇ Design ◇ Fine Art</h2>
+	<small class='details'>Digital painting by Brian Coutts. Original photo by Joseph Eid.</small>
 
 </section>
