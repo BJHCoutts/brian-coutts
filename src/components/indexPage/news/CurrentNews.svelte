d507074b63652d4021f6f84ffc2204f9a5321dff
Brian Coutts <bjhcoutts@gmail.com>
2022-02-01 17:06:29 -0800

    resume link added

--- src/components/indexPage/news/CurrentNews.svelte
+++ src/components/indexPage/news/CurrentNews.svelte
@@ -4,4 +4,10 @@
 
 	<p>This site is currently WIP</p>
 	
+	<p>
+		<a href="https://drive.google.com/file/d/18dGW66hD8Ku0HlGGd6eWdtIZHcdRVq_a/view?usp=sharing">Link to Resume PDF</a>
+	</p>
+	<p>
+		<a href="https://drive.google.com/file/d/1fN5i6lWL9IWDJQ2zZJ3D8HV--x1ErYSz/view?usp=sharing">Link to Visual Portfolio PDF</a>
+	</p>
 </article>
