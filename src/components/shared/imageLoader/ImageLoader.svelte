e53bce8c2ee0fd7c44e4bae4fc230eab55d6056e
Brian Coutts <bjhcoutts@gmail.com>
2022-02-08 18:03:15 -0800

    image lazy loader + native lazy load bypass

---
+++ src/components/shared/imageLoader/ImageLoader.svelte
@@ -0,0 +1,27 @@
+<script lang='ts'>
+import { onMount } from "svelte";
+
+
+	import IntersectionObserver from "../intersectionalObserver/IntersectionObserver.svelte";
+	import Image from "./Image.svelte";
+
+	export let src
+	export let alt
+
+	let nativeLazyLoading = false
+
+	onMount( () => {
+		if ('loading' in HTMLImageElement.prototype) {
+			nativeLazyLoading = true
+		}
+	})
+
+</script>
+
+<IntersectionObserver once={false} let:intersecting={intersecting} >
+
+	{#if intersecting || nativeLazyLoading}
+		<Image {alt} {src} />
+	{/if}
+
+</IntersectionObserver>
