7b417b0a549907c4ca4c9d0556fa5f01c840c610
Brian Coutts <bjhcoutts@gmail.com>
2022-02-14 17:30:31 -0800

    new idea, animated inv parent div

--- src/components/shared/featured/MultiImage.svelte
+++ src/components/shared/featured/MultiImage.svelte
@@ -4,6 +4,12 @@
 		background-color: grey;
 		width: 15%;
 		height: 15%;
+		position: absolute;
+	}
+
+	.animatedContainer {
+		position: relative;
+		border: 2px solid red;
 	}
 
 	.background {
@@ -19,14 +25,36 @@
 	}
 
 	.image1{
+		top: 0;
+		left:0;
+	}
 
+	.image2{
+		top: 0;
+		right:0;
 	}
 	
+	.image3{
+		bottom: 0;
+		left:0;
+	}
+
+	.image4{
+		bottom: 0;
+		right:0;
+	}
+
 </style>
 
 <div class='container'>
 
 	<div class='background' />
-	<img src='image1' alt='' />
+	<div class="animatedContainer">
+		<img src='image1' alt='' />
+		<img src='image2' alt='' />
+		<img src='image3' alt='' />
+		<img src='image4' alt='' />
+
+	</div>
 
 </div>
