b4c5684efb130276ec109f2c6f75021d4e9347ff
Brian Coutts <bjhcoutts@gmail.com>
2022-02-16 13:44:55 -0800

    spring animations added onto menu

--- src/components/menu/Menu.svelte
+++ src/components/menu/Menu.svelte
@@ -1,20 +1,10 @@
 <script lang='ts'>
-	
-	import { menuIsOpen } from '../../stores/ui'
 
-	const handleClick = () => {
-		menuIsOpen.set(!$menuIsOpen)
-	}
+	import { handleMenuClick, innerMenuSpring, outerMenuSpring } from "../../stores/ui";
 
 </script>
-
 <style>
 
-	nav {
-
-	
-	}
-	
 	ul {
 		list-style-type: none;
 	}
@@ -30,12 +20,9 @@
 
 	.container-inner {
 		place-items: center start;
-		transition: clip-path 0.3s ease-in;
 		background-color: var(--dark-overlay);
 		position: fixed;
 		inset: 0;
-		clip-path: circle(0px at 0% 50%);
-		transition: clip-path 0.3s ease-in;
 		background-color: var(--dark-overlay);
 	}
 
@@ -43,30 +30,20 @@
 		z-index: 3;
 		position: fixed;
 		inset: 0;
-		clip-path: circle(0px at 0% 50%);
 		background-color: var(--light-overlay);
-		transition: clip-path 0.2s ease-in;
-	}
-
-	.open-inner{
-		clip-path: circle(70% at 0% 50%);
-	}
-
-	.open-outter{
-		clip-path: circle(80% at 0% 50%);
 	}
 
 </style>
 
 <section
 	class='container-outter' 
-	on:click={handleClick} 
-	class:open-outter='{$menuIsOpen}'
+	on:click={handleMenuClick} 
+	style={`clip-path: circle(${$outerMenuSpring}% at 0% 50%);`}
 >
 
 	<nav 
 		class='main-grid-layout container-inner' 
-		class:open-inner='{$menuIsOpen}'
+		style={`clip-path: circle(${$innerMenuSpring}% at 0% 50%);`}
 	>
 		<ul class='main-grid-center' style='overflow: visible;'>
 
--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -1,7 +1,24 @@
-import { writable } from "svelte/store"
+import { writable } from 'svelte/store'
+import { spring } from 'svelte/motion'
 
 export const menuIsOpen = writable(false)
 
 export const imageModalIsOpen = writable(false)
 
 export const imageModalUrl = writable('')
+
+export const innerMenuSpring = spring(0,{
+	stiffness: .1,
+	damping: .7,
+});
+
+export const outerMenuSpring = spring(0,{
+	stiffness: .2,
+	damping: .8,
+});
+
+export const handleMenuClick = ():void => {
+	menuIsOpen.update(menuIsOpenValue => !menuIsOpenValue)
+	innerMenuSpring.update(innerMenuSpringValue => innerMenuSpringValue === 0 ? 70 : 0)
+	outerMenuSpring.update(outerMenuSpringValue => outerMenuSpringValue === 0 ? 80 : 0)
+}
